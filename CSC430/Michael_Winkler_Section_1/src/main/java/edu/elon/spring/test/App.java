55614c5f2303dcad8a35afcec876a225c8eaf90e
Michael <mwinkler3@elon.edu>
2016-02-09 11:54:27 -0500

    Forgot to export the folder for last commit - fixed

---
+++ CSC430/Michael_Winkler_Section_1/src/main/java/edu/elon/spring/test/App.java
@@ -0,0 +1,19 @@
+package edu.elon.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("/edu/elon/spring/test/beans/beans.xml");
+				
+		Person person = (Person) context.getBean("person");
+		person.speak();
+				
+		((ClassPathXmlApplicationContext) context).close();
+	}
+
+}
---
+++ CSC430/Michael_Winkler_Section_1/src/main/java/edu/elon/spring/test/Person.java
@@ -0,0 +1,7 @@
+package edu.elon.spring.test;
+
+public class Person {
+	public void speak(){
+		System.out.println("Hello! I'm a person");
+	}
+}
