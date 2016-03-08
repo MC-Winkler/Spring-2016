5035edf2301676f7dcbbfef5bd0b81be3fee6dc7
Michael Winkler <mwinkler3@elon.edu>
2016-03-08 13:50:33 -0500

    Finish evil hangman

--- CSC335/Winkler_Padula_Evil_Hangman.py
+++ CSC335/Winkler_Padula_Evil_Hangman.py
@@ -39,73 +39,76 @@ def getRunningTotal():
         return True
     else:
         return False
+
+def afterGuessOutput(guesses,lettersGuessed,currentWord,allWordsOfLength):
+    print ("Gueeses remaining: " + str(guesses))
+    print("Letters guessed: " + str(lettersGuessed))
+    print("Word currently is: " + currentWord)
+    print("Words left in list: "+str(len(allWordsOfLength)))
+
+    
         
     
 ourDictionaryList = getDictionaryAsList("testDictionary.txt")
-#print (getWordLength(ourDictionaryList))
-#print (getGuesses())
-#print (getRunningTotal())
 
-allWordsOfChosenLength = getWordLength(ourDictionaryList)
-guessesRemaining = getGuesses()
-wantsRunningTotal = getRunningTotal()
-lettersGuessed = []
-currentWord = ""
-playerWon = False
-for i in range(len(allWordsOfChosenLength[0])):
-    currentWord = currentWord + "-"
-    
-while guessesRemaining > 0 and playerWon is not True:
-    #print("You have " + str(guessesRemaining) + " guesses left.")
-    #if (wantsRunningTotal):
-     #   print("There is/are " + str(len(allWordsOfChosenLength)) + " word/s left in the list." )
-    #print("The letters you've guessed: " + str(lettersGuessed))
-    #print("The word, with the letters you've guessed revealed: " + currentWord)
-    
-    user_input = ""
-    while len(user_input) != 1 or user_input in lettersGuessed or not (re.search('[a-zA-Z]+', user_input)):
-        user_input = input("Guess a letter: ")
-    lettersGuessed.append(user_input)
+wantsToPlay = "yes"
+while wantsToPlay == "yes":
+    allWordsOfChosenLength = getWordLength(ourDictionaryList)
+    guessesRemaining = getGuesses()
+    wantsRunningTotal = getRunningTotal()
+    lettersGuessed = []
+    currentWord = ""
+    playerWon = False
+    for i in range(len(allWordsOfChosenLength[0])):
+        currentWord = currentWord + "-"
+    allDashes = ""
+    for i in range(len(allWordsOfChosenLength[1])):
+        allDashes = allDashes + "-"
         
-    families = {}
-    for word in allWordsOfChosenLength:
-        key = currentWord
-        if word.find(user_input) > -1:
+    while guessesRemaining > 0 and playerWon is not True:   
+        user_input = ""
+        while len(user_input) != 1 or user_input in lettersGuessed or not (re.search('[a-zA-Z]+', user_input)):
+            user_input = input("Guess a letter: ")
+        lettersGuessed.append(user_input)
+            
+        families = {}
+        for word in allWordsOfChosenLength:
+            key = currentWord
+            if word.find(user_input) > -1:
+                for i in range(len(word)):
+                    if word[i] == user_input:
+                        key = key[:i] + user_input + key[(i+1):]
+            if key not in families:
+                families[key] = []
+            families[key].append(word)
+            
+        longestLengthFound = 0
+        longestKey = ""
+        if(guessesRemaining == 1 and families[allDashes] != None):
+            longestKey = allDashes
+        else:
+            for key in families:
+                if len(families[key]) > longestLengthFound:
+                    longestLengthFound = len(families[key])
+                    longestKey = key
+        
+        currentWord = longestKey
+        allWordsOfChosenLength = families[longestKey]
+        
+    
+        if longestKey == allDashes:
+            guessesRemaining = guessesRemaining-1
+         
+        afterGuessOutput(guessesRemaining,lettersGuessed,currentWord,allWordsOfChosenLength)
+        
+        
+        if len(families[longestKey]) == 1 and currentWord.find("-") == -1:
+            playerWon = True
+            print("Hey good job you won, the word was " + currentWord)
+            wantsToPlay = input("Do you want to play again? yes/no: " )
+        
+        if guessesRemaining == 0:
+            print("You lose and you should feel bad. The word was " + allWordsOfChosenLength.pop())
+            wantsToPlay = input("Do you want to play again? yes/no: " )
 
-            print("word = " + word)
-            for i in range(len(word)):
-                if word[i] == user_input:
-                    print("In the inner if statement, i = " + str(i) )
-                    print("key[:i] = " + key[:i])
-                    print("user_input = " + user_input)
-                    print("key[(i+1)] = " + key[(i+1):])                    
-                    key = key[:i] + user_input + key[(i+1):]
-        print("key got updated to " + key)
-        if families.get(key) is None:
-            families[key] = []
-        tempFamilies = families.get(key)
-        tempFamilies.append(word)
-        families[key] = tempFamilies
         
-    #print("families = " + str(families))
-    longestLengthFound = 0
-    longestKey = ""
-    for key in families:
-        if len(families.get(key)) > longestLengthFound:
-            longestLengthFound = len(families.get(key))
-            longestKey = key
-    
-    currentWord = longestKey
-    allWordsOfChosenLength = families.get(longestKey)
-   # if longestKey == allDashes:
-    #    guessesRemaining -= 1
-    
-    print("After everything ran for one loop iteratrion, families = " + str(families))
-    print("After everything ran for one loop iteration, currentWord = " + currentWord)
-    
-    if len(families.get(longestKey)) == 1 and currentWord.find("-") == -1:
-        playerWon = True
-        print("Hey good job you won, the word was " + currentWord)
-    
-    if guessesRemaining == 0:
-        print("You lose and you should feel bad. The word was " + allWordsOfChosenLength.pop())
