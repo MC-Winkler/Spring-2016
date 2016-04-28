c4fb135437066cd1f83dd9b06d51afb380a7c8b0
Michael <mwinkler3@elon.edu>
2016-04-27 20:48:42 -0400

    Add csc331 GraphsModified3

---
+++ CSC331/GraphsModified3/src/DepthFirstSearcher.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class DepthFirstSearcher {
+	
+	/*
+	 * Depth-first search exploration
+	 */
+	public void explore(Graph<Vertex> g, Vertex v) {
+	
+	}
+
+	/*
+	 * Depth-First search
+	 */
+	public void depthFirstSearch(Graph<Vertex> g){
+	
+	}
+	
+	
+	
+	public void printDFSDataStructures(){
+		
+	}
+}
