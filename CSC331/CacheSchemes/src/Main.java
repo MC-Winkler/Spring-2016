9c165cc5a6c59543dc5eb5d6dc95912e022477bf
Michael Winkler <mwinkler3@elon.edu>
2016-05-11 23:52:26 -0400

    5/11/16

--- CSC331/CacheSchemes/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		SchemeComparisons compare = new SchemeComparisons();
-		compare.userCompare();
-		//compare.comparePresetValues();
-	}
-}
